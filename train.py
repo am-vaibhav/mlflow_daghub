12baba904adb7bcb6e7fb99563dc2fdedf8eb00c
Vaibhav Kumar <vaibhav.kumar@diggrowth.com>
2026-08-13 16:31:31 +0530

    initial commit

---
+++ train.py
@@ -0,0 +1,50 @@
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=42
+)
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("iris-classification")
+
+n_estimators = 100
+max_depth = 5
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=n_estimators, max_depth=max_depth, random_state=23
+    )
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, predictions)
+    f1 = f1_score(y_test, predictions, average="weighted")
+
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("f1_score", f1)
+    mlflow.sklearn.log_model(model, name="random_forest_model")
+
+    cm = confusion_matrix(y_test, predictions)
+    plt.figure(figsize=[10, 5])
+    sns.heatmap(cm, annot=True, cmap="Blues", fmt="g")
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+
+    mlflow.log_artifact(__file__)
+    mlflow.set_tag("author","vaibhav")
+    mlflow.set_tag("model","random_forest")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"F1 Score: {f1:.4f}")
